d0599ed2711dee5506950ce6d7206d3cee6bb0c1
bhoopalj <bhoopaljanga@gmail.com>
2022-04-26 23:52:39 +0530

    update use

--- src/app/views/docs/products/product.component.ts
+++ src/app/views/docs/products/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'; 
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
  
 
 import { ShoppingCartService, CartItem, Totals } from '../shopping-cart.service';
@@ -12,8 +12,17 @@ import { ShoppingCartService, CartItem, Totals } from '../shopping-cart.service'
 export class ProdcutComponent implements OnInit   {
 
   name = 'Shopping Cart';
+  processing = false;
   cartState$ = this.shoppingCartService.state$;
-  constructor(private shoppingCartService: ShoppingCartService) { }
+  constructor(private shoppingCartService: ShoppingCartService,
+    private http: HttpClient,
+    private route: ActivatedRoute) {
+
+      this.route.params.subscribe(params=>{
+        this.getPaymentDetails();
+      })
+
+   }
 
   ngOnInit(): void { 
   }
@@ -24,6 +33,30 @@ export class ProdcutComponent implements OnInit   {
 
   remove(item: CartItem): void {
     this.shoppingCartService.removeCartItem(item);
+  }
+
+  paymentDetails:any;
+  getPaymentDetails(){ 
+
+    
+  const queryParams = this.route.snapshot.queryParams;
+
+  const paymentId = queryParams['paymentId'];
+  if(!paymentId)
+    return
+  const token = queryParams['token'];
+  const PayerID = queryParams['PayerID'];
+  this.processing = true;
+     
+      this.http.get('https://cnsmzwycy43jf7c6egulpxvtxm0xtdxc.lambda-url.ap-south-1.on.aws',{ 
+        params:{"paymentId": paymentId as string,token:token as string, payerID: PayerID as string}
+      }).subscribe((details:any)=>{
+        this.paymentDetails = details;
+        this.processing = false;
+       console.log(details)
+      })
+    
+    //paymentId=PAYID-MJUCVCA9W074514VV412114R&token=EC-8BE00145BD285080W&PayerID=Z4J5Y9667WSNY
   }
 
 }
